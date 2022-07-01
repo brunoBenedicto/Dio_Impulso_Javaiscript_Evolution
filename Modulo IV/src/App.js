99314f55af1b7dc6f751ae6a06e2819451c4304b
brunoBenedicto <brunobenedicto.rocha@gmail.com>
2022-06-30 22:28:52 -0300

    configuração webpack

---
+++ Modulo IV/src/App.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+
+
+const App =() => {
+
+    return(
+        <div>
+            <p> Digital inovation one</p>
+            <p> bem vindo a nossa aula</p>
+            <p>ola</p>
+            <br/>
+        </div>
+    )
+}
+
+export default App;
