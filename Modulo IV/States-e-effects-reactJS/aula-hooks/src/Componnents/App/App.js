cdadb5f8caada76a0be6bdcf1e02339a55bd309f
brunoBenedicto <brunobenedicto.rocha@gmail.com>
2022-07-10 12:19:46 -0300

    componnent with useState

--- Modulo IV/States-e-effects-reactJS/aula-hooks/src/Componnents/App/App.js
+++ Modulo IV/States-e-effects-reactJS/aula-hooks/src/Componnents/App/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import Counter from '../Counter/Counter';
+import SmartCounter from '../SmartCounter/SmartCounter';
 function App() {
   return (
     <>
       <h1>ae meu vei</h1>
       <Counter/>
+      <SmartCounter/>
     </>
   );
 }
